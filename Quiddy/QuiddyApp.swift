f5ff446d1fdbbc6d5f58edb5b5449f6acaf1a4c9
stephan <ardistephan@gmail.com>
2025-11-05 14:42:28 +0800

    Initial Commit

---
+++ Quiddy/QuiddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiddyApp.swift
+//  Quiddy
+//
+//  Created by stephan on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct QuiddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
